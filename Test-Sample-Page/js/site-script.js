f7e26dc695976f93431f244325da6db622c309ce
Waseem-webdev <waseem.webdev@gmail.com>
2017-01-30 02:25:11 +0000

    Add files via upload

---
+++ Test-Sample-Page/js/site-script.js
@@ -0,0 +1,64 @@
+
+$(document).ready(function(){
+
+
+//Responsive menu dropdown
+$( ".cross" ).hide();
+$( ".menu" ).hide();
+
+$( ".hamburger" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
+
+
+
+//smooth scroll
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+})
+
+
+
+//Google mao location
+function myMap() {
+
+  var myCenter = new google.maps.LatLng(37.422358,-122.084446);
+  var mapCanvas = document.getElementById("googleMap");
+  var mapOptions = {center: myCenter, zoom: 15};
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  var marker = new google.maps.Marker({position:myCenter});
+  marker.setMap(map);
+}
+
+
+
+
+
+
+
+
+            
